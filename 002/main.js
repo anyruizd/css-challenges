9c770b9b081b5557ef51897e90693f838058f96f
anyruizd <aruiz@hugeinc.com>
2018-07-21 20:55:36 -0500

    Hamburger works but I have to optimize, if it is possible

--- 002/main.js
+++ 002/main.js
@@ -1,12 +1,13 @@
 const trigger = document.querySelector('.hamburger')
 trigger.addEventListener('click', (event) => {
   const clickedElement = event.target.closest('.hamburger')
-  clickedElement.classList.remove('animation-off')
-  if (clickedElement.classList.contains('menu--open')) {
+  clickedElement.classList.remove('animation--off')
+  clickedElement.classList.toggle('menu--open')
+  /* if (clickedElement.classList.contains('menu--open')) {
     clickedElement.classList.add('menu--close')
     clickedElement.classList.remove('menu--open')
   } else {
     clickedElement.classList.add('menu--open')
     clickedElement.classList.remove('menu--close')
-  }
+  } */
 })
